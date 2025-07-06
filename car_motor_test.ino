6b106f7e73e58b870f42b7506da4e3a2e077ca9d
mazerunnerlatam <mazerunner.latam@gmail.com>
2025-07-05 22:32:41 -0300

    Update car_motor_test.ino

--- car_motor_test.ino
+++ car_motor_test.ino
@@ -54,33 +54,69 @@ void setup() {
 
 
 }
+
+
+
 void loop() {
-  int speed_slow = 96;    
-  int speed_fast =  speed_slow + 32;
-/*  
-int cantidad_de_repeticiones = 10;
-int valor_minimo = 50;
-for (int i = 0; i < cantidad_de_repeticiones; i++) {
-  Serial.print("going forward: LEFT slow: ");
-  Serial.print(speed_slow);
-  Serial.print(" - RIGHT slow: ");
-  Serial.println(speed_slow);
-  motor_left_forward(speed_slow);
-  motor_right_forward(speed_slow);
-  delay(3000);
-  Serial.println("Stop");
-  motor_left_stop();  
-  motor_right_stop();
-  delay(1000);
+
+  averiguar_vel_minima();
+  /*
+  22:27:30.868 -> going forward: LEFT slow: 128 - RIGHT slow: 128
+  22:27:33.871 -> Stop
+  22:27:34.874 -> going forward: LEFT slow: 120 - RIGHT slow: 120
+  22:27:37.912 -> Stop
+  22:27:38.872 -> going forward: LEFT slow: 112 - RIGHT slow: 112
+  22:27:41.916 -> Stop
+  22:27:42.885 -> going forward: LEFT slow: 104 - RIGHT slow: 104
+  22:27:45.919 -> Stop
+  22:27:46.893 -> going forward: LEFT slow: 96 - RIGHT slow: 96
+  22:27:49.883 -> Stop
+  22:27:50.883 -> going forward: LEFT slow: 88 - RIGHT slow: 88
+  22:27:53.908 -> Stop
+  22:27:54.906 -> going forward: LEFT slow: 80 - RIGHT slow: 80 - fue el minimo que registre para ambos motores
+  22:27:57.905 -> Stop
+  22:27:58.893 -> going forward: LEFT slow: 72 - RIGHT slow: 72 - Solo se movia Motor Izquierdo
+  22:28:01.884 -> Stop
+  */
+  while(1)
+  {
+  Serial.println("While LOOP 1...");
+  delay(15000);
+  }  
+  check_motor_connections_speed();
+}
+
+void averiguar_vel_minima() {
+  //int speed_slow = 128;    
+  int speed_slow = 88;    
   
-  speed_slow -= 8; // Decrementa speed_slow en 8
-  
-  // Opcional: puedes agregar una condición para evitar que speed_slow sea menor que un valor mínimo
-  if (speed_slow < valor_minimo) {
-    speed_slow = valor_minimo;
+  int cantidad_de_repeticiones = 10;
+  int valor_minimo = 50;
+  for (int i = 0; i < cantidad_de_repeticiones; i++) {
+    Serial.print("going forward: LEFT slow: ");
+    Serial.print(speed_slow);
+    Serial.print(" - RIGHT slow: ");
+    Serial.println(speed_slow);
+    motor_left_forward(speed_slow);
+    motor_right_forward(speed_slow);
+    delay(3000);
+    Serial.println("Stop");
+    motor_left_stop();  
+    motor_right_stop();
+    delay(1000);
+    
+    speed_slow -= 8; // Decrementa speed_slow en 8
+    
+    // Opcional: puedes agregar una condición para evitar que speed_slow sea menor que un valor mínimo
+    if (speed_slow < valor_minimo) {
+      speed_slow = valor_minimo;
+    }
   }
 }
-*/
+
+void check_motor_connections_speed() {
+  int speed_slow = 128;    
+  int speed_fast =  speed_slow + 32;
 
   Serial.print("going forward: LEFT slow: ");
   Serial.print(speed_slow);
